e7f6146a8b006b8698448a0cd9260db86aa02819
Saad <think2060@gmail.com>
2022-07-16 18:24:09 +0300

    Finished lesson 11

---
+++ Assets/Scripts/CinematicTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject cinematicCamera;
+    [SerializeField] GameObject cutscene;
+    [SerializeField] GameObject fpsController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        fpsController.SetActive(false);
+        cinematicCamera.SetActive(true);
+        cutscene.SetActive(true);
+    }
+
+}
